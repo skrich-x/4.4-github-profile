57db73013afc7df42e5fefd7279b2fb4fd2729c2
Kyle Richardson <s.k.richardson7@gmail.com>
2015-05-28 15:43:13 -0400

    Authentication Complete - data fetched from initial ajax request

--- scripts/main.js
+++ scripts/main.js
@@ -10,5 +10,16 @@
     }).then(function(user) {
       console.log(user);
     });
+
+   var code = window.location.href.match(/\?code=(.*)/)[1];
+    if(code) {
+      $.getJSON('http://localhost:9999/authenticate/'+code, function(data) {
+       console.log(data.token);
+      });
+    }
+  });
+
+  $('button').on('click', function(e){
+    window.location.replace('https://github.com/login/oauth/authorize?client_id=5b1c415eb446deb20032');
   });
 })();
--- token.js
+++
@@ -1,6 +0,0 @@
-(function(){
-  'use strict'
-
-  window.GITHUB_TOKEN = '7c0888a18edd810b763603387b9eea934e28a6b9';
-
-})();
