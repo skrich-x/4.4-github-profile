4eae50c23b768b663cf2d92e233e0eac4fa9ea75
Kyle Richardson <s.k.richardson7@gmail.com>
2015-05-29 21:39:35 -0400

    Added handlebars templates for User Information still getting "bad code error" from node server.js

--- dist/scripts/templates.js
+++ dist/scripts/templates.js
@@ -1,4 +1,33 @@
 this["JST"] = this["JST"] || {};
 this["JST"]["application"] = Handlebars.template({"compiler":[6,">= 2.0.0-beta.1"],"main":function(depth0,helpers,partials,data) {
-    return "<h1>Hello</h1>\n";
+    return "";
+},"useData":true});
+this["JST"]["user"] = Handlebars.template({"compiler":[6,">= 2.0.0-beta.1"],"main":function(depth0,helpers,partials,data) {
+    var helper, alias1=helpers.helperMissing, alias2="function", alias3=this.escapeExpression;
+
+  return "\n<img src=\""
+    + alias3(((helper = (helper = helpers.avatar_url || (depth0 != null ? depth0.avatar_url : depth0)) != null ? helper : alias1),(typeof helper === alias2 ? helper.call(depth0,{"name":"avatar_url","hash":{},"data":data}) : helper)))
+    + "\" alt=\"\" />\n\n<h1>"
+    + alias3(((helper = (helper = helpers.name || (depth0 != null ? depth0.name : depth0)) != null ? helper : alias1),(typeof helper === alias2 ? helper.call(depth0,{"name":"name","hash":{},"data":data}) : helper)))
+    + "</h1>\n<h2>"
+    + alias3(((helper = (helper = helpers.login || (depth0 != null ? depth0.login : depth0)) != null ? helper : alias1),(typeof helper === alias2 ? helper.call(depth0,{"name":"login","hash":{},"data":data}) : helper)))
+    + "</h2>\n\n<hr>\n\n<span class=\"octicon octicon-location\"></span><span>"
+    + alias3(((helper = (helper = helpers.location || (depth0 != null ? depth0.location : depth0)) != null ? helper : alias1),(typeof helper === alias2 ? helper.call(depth0,{"name":"location","hash":{},"data":data}) : helper)))
+    + "</span>\n<span class=\"octicon octicon-clock\"></span>\n<span>Joined On"
+    + alias3(((helper = (helper = helpers.created_at || (depth0 != null ? depth0.created_at : depth0)) != null ? helper : alias1),(typeof helper === alias2 ? helper.call(depth0,{"name":"created_at","hash":{},"data":data}) : helper)))
+    + "</span>\n\n<hr>\n\n<a href=\""
+    + alias3(((helper = (helper = helpers.followers_url || (depth0 != null ? depth0.followers_url : depth0)) != null ? helper : alias1),(typeof helper === alias2 ? helper.call(depth0,{"name":"followers_url","hash":{},"data":data}) : helper)))
+    + "\"><h3>"
+    + alias3(((helper = (helper = helpers.followers || (depth0 != null ? depth0.followers : depth0)) != null ? helper : alias1),(typeof helper === alias2 ? helper.call(depth0,{"name":"followers","hash":{},"data":data}) : helper)))
+    + "</h3></a>\n<span>Followers</span>\n\n<a href=\""
+    + alias3(((helper = (helper = helpers.starred_url || (depth0 != null ? depth0.starred_url : depth0)) != null ? helper : alias1),(typeof helper === alias2 ? helper.call(depth0,{"name":"starred_url","hash":{},"data":data}) : helper)))
+    + "\">"
+    + alias3(((helper = (helper = helpers.starred || (depth0 != null ? depth0.starred : depth0)) != null ? helper : alias1),(typeof helper === alias2 ? helper.call(depth0,{"name":"starred","hash":{},"data":data}) : helper)))
+    + "</a>\n<span>starred</span>\n\n<a href=\""
+    + alias3(((helper = (helper = helpers.following_url || (depth0 != null ? depth0.following_url : depth0)) != null ? helper : alias1),(typeof helper === alias2 ? helper.call(depth0,{"name":"following_url","hash":{},"data":data}) : helper)))
+    + "\"><h3>"
+    + alias3(((helper = (helper = helpers.following || (depth0 != null ? depth0.following : depth0)) != null ? helper : alias1),(typeof helper === alias2 ? helper.call(depth0,{"name":"following","hash":{},"data":data}) : helper)))
+    + "</h3></a>\n<span>Following</span>\n\n<hr>\n\n<h3>"
+    + alias3(((helper = (helper = helpers.organizations_url || (depth0 != null ? depth0.organizations_url : depth0)) != null ? helper : alias1),(typeof helper === alias2 ? helper.call(depth0,{"name":"organizations_url","hash":{},"data":data}) : helper)))
+    + "</h3>\n";
 },"useData":true});
--- scripts/main.js
+++ scripts/main.js
@@ -3,23 +3,39 @@
 
   $(document).ready(function(){
     $.ajax({
-      url: "https://api.github.com/user",
-      headers: {
+      url: "https://api.github.com/users/skrich-x",
+       headers: {
         "Authorization": "token " + GITHUB_TOKEN
-      }
+       }
+
     }).then(function(user) {
+      $('.user').prepend(JST['user'](user));
       console.log(user);
-    });
-
-   var code = window.location.href.match(/\?code=(.*)/)[1];
-    if(code) {
-      $.getJSON('http://localhost:9999/authenticate/'+code, function(data) {
-       console.log(data.token);
-      });
-    }
   });
+});
 
-  $('button').on('click', function(e){
-    window.location.replace('https://github.com/login/oauth/authorize?client_id=5b1c415eb446deb20032');
+  $(document).ready(function(){
+    $.ajax({
+    url: "https://api.github.com/users/skrich-x/repos",
+     headers: {
+    "Authorization": "token " + GITHUB_TOKEN
+
+}
+  }).then(function(repos) {
+    $('body').prepend(JST['application'](repos));
+    console.log(repos);
   });
+});
+  // $(document).ready(function(){
+  //  var code = window.location.href.match(/\?code=(.*)/)[1];
+  //   if(code) {
+  //     $.getJSON('http://localhost:9999/authenticate/'+code, function(data) {
+  //       console.log('data is',data)
+  //     });
+  //   }
+  // });
+  //
+  // $('button').on('click', function(e){
+  //   window.location.replace('https://github.com/login/oauth/authorize?client_id=5b1c415eb446deb20032');
+//  });
 })();
