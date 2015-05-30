4ba8b30d04a0430f613062947699f63be52472d8
Kyle Richardson <s.k.richardson7@gmail.com>
2015-05-29 23:18:22 -0400

    Added styling to user section

--- dist/scripts/templates.js
+++ dist/scripts/templates.js
@@ -30,27 +30,27 @@ this["JST"]["repos"] = Handlebars.template({"1":function(depth0,helpers,partials
 this["JST"]["user"] = Handlebars.template({"compiler":[6,">= 2.0.0-beta.1"],"main":function(depth0,helpers,partials,data) {
     var helper, alias1=helpers.helperMissing, alias2="function", alias3=this.escapeExpression;
 
-  return "\n<img src=\""
+  return "<section class = 'userContainer'>\n  <div class = \"avatar\">\n    <img src=\""
     + alias3(((helper = (helper = helpers.avatar_url || (depth0 != null ? depth0.avatar_url : depth0)) != null ? helper : alias1),(typeof helper === alias2 ? helper.call(depth0,{"name":"avatar_url","hash":{},"data":data}) : helper)))
-    + "\" alt=\"\" />\n\n<h1>"
+    + "\" alt=\"\" />\n  <div>\n\n  <h1>"
     + alias3(((helper = (helper = helpers.name || (depth0 != null ? depth0.name : depth0)) != null ? helper : alias1),(typeof helper === alias2 ? helper.call(depth0,{"name":"name","hash":{},"data":data}) : helper)))
-    + "</h1>\n<h2>"
+    + "</h1>\n  <h2>"
     + alias3(((helper = (helper = helpers.login || (depth0 != null ? depth0.login : depth0)) != null ? helper : alias1),(typeof helper === alias2 ? helper.call(depth0,{"name":"login","hash":{},"data":data}) : helper)))
-    + "</h2>\n\n<hr>\n\n<span class=\"octicon octicon-location\"></span><span>"
+    + "</h2>\n\n  <hr>\n\n  <span class=\"octicon octicon-location\"></span><span>"
     + alias3(((helper = (helper = helpers.location || (depth0 != null ? depth0.location : depth0)) != null ? helper : alias1),(typeof helper === alias2 ? helper.call(depth0,{"name":"location","hash":{},"data":data}) : helper)))
-    + "</span>\n<span class=\"octicon octicon-clock\"></span>\n<span>Joined On"
+    + "</span>\n  <span class=\"octicon octicon-clock\"></span>\n  <span>Joined On"
     + alias3(((helper = (helper = helpers.created_at || (depth0 != null ? depth0.created_at : depth0)) != null ? helper : alias1),(typeof helper === alias2 ? helper.call(depth0,{"name":"created_at","hash":{},"data":data}) : helper)))
-    + "</span>\n\n<hr>\n\n<a href=\""
+    + "</span>\n\n  <hr>\n\n  <a href=\""
     + alias3(((helper = (helper = helpers.followers_url || (depth0 != null ? depth0.followers_url : depth0)) != null ? helper : alias1),(typeof helper === alias2 ? helper.call(depth0,{"name":"followers_url","hash":{},"data":data}) : helper)))
     + "\"><h3>"
     + alias3(((helper = (helper = helpers.followers || (depth0 != null ? depth0.followers : depth0)) != null ? helper : alias1),(typeof helper === alias2 ? helper.call(depth0,{"name":"followers","hash":{},"data":data}) : helper)))
-    + "</h3></a>\n<span>Followers</span>\n\n<a href=\""
+    + "</h3></a>\n  <span>Followers</span>\n\n  <a href=\""
     + alias3(((helper = (helper = helpers.starred_url || (depth0 != null ? depth0.starred_url : depth0)) != null ? helper : alias1),(typeof helper === alias2 ? helper.call(depth0,{"name":"starred_url","hash":{},"data":data}) : helper)))
     + "\">"
     + alias3(((helper = (helper = helpers.starred || (depth0 != null ? depth0.starred : depth0)) != null ? helper : alias1),(typeof helper === alias2 ? helper.call(depth0,{"name":"starred","hash":{},"data":data}) : helper)))
-    + "</a>\n<span>starred</span>\n\n<a href=\""
+    + "</a>\n  <span>starred</span>\n\n  <a href=\""
     + alias3(((helper = (helper = helpers.following_url || (depth0 != null ? depth0.following_url : depth0)) != null ? helper : alias1),(typeof helper === alias2 ? helper.call(depth0,{"name":"following_url","hash":{},"data":data}) : helper)))
     + "\"><h3>"
     + alias3(((helper = (helper = helpers.following || (depth0 != null ? depth0.following : depth0)) != null ? helper : alias1),(typeof helper === alias2 ? helper.call(depth0,{"name":"following","hash":{},"data":data}) : helper)))
-    + "</h3></a>\n<span>Following</span>\n\n<hr>\n\n<h4>Organizations</h4>\n\n<a href=\"/TIY-GVL-FEE-2015-May\" aria-label=\"TIY-GVL-FEE-2015-May\"><img alt=\"@TIY-GVL-FEE-2015-May\" src=\"https://avatars0.githubusercontent.com/u/11824325?v=3&amp;s=84\"></a>\n\n\n<a href=\"/TechClimber\" aria-label=\"TechClimber\"><img alt=\"@TechClimber\" data-user=\"12204206\" src=\"https://avatars0.githubusercontent.com/u/12204206?v=3&amp;s=84\"></a>\n";
+    + "</h3></a>\n  <span>Following</span>\n\n  <hr>\n  <div class = \"organization\">\n    <h4>Organizations</h4>\n\n    <a href=\"/TIY-GVL-FEE-2015-May\" aria-label=\"TIY-GVL-FEE-2015-May\"><img alt=\"@TIY-GVL-FEE-2015-May\" src=\"https://avatars0.githubusercontent.com/u/11824325?v=3&amp;s=84\"></a>\n\n\n    <a href=\"/TechClimber\" aria-label=\"TechClimber\"><img alt=\"@TechClimber\" data-user=\"12204206\" src=\"https://avatars0.githubusercontent.com/u/12204206?v=3&amp;s=84\"></a>\n  </div>\n\n</section>\n";
 },"useData":true});
